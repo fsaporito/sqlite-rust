e1574851b0922e527630316985db6b2753094f34
fsaporito <sap93@gmail.com>
2024-11-20 18:07:06 +0100

    Add metacommands

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,32 @@ use std::io::{self, Write};
 fn print_help() {
     println!("#### SQLITE Rust Clone ###\n");
     println!("Helper \n");
-    println!("Available Commands:\n");
+    println!("Available Meta Commands: [Metacommands start with a dot] \n");
     println!("\t .help, .h => this prompt\n");
     println!("\t .quit, .q => quit program prompt\n");
 }
 
 fn print_prompt() {
     print!("db > ");
-    io::stdout().flush().expect("Failed to flush stdout")
+    io::stdout().flush().expect("Failed to flush stdout");
+}
+
+fn is_metacommand(input_command: &str) -> bool {
+    return input_command.starts_with('.');
+}
+
+fn sql_parser(sql_command: &str) {
+    println!("Received SQL command: {}\n", sql_command);
+
+    if sql_command.starts_with("insert") {
+        println!("The received SQL command is an INSERT statement");
+    } else if sql_command.starts_with("delete") {
+        println!("The received SQL command is an DELETE statement");
+    } else if sql_command.starts_with("select") {
+        println!("The received SQL command is an SELECT statement");
+    } else {
+        println!("Unknown SQL command: {}\n", sql_command);
+    }
 }
 
 fn main() {
@@ -28,18 +46,22 @@ fn main() {
 
         let input_command = input_command.trim();
 
-        println!("Received command is: {}\n", input_command);
+        println!("Received input is: {}\n", input_command);
 
-        match input_command {
-            ".quit" | ".q" => {
-                    println!("Closing down application. Bye\n");
-                    break;
-                },
-            ".help" | ".h" => {
-                print_help();
-                continue;
-                },
-            _ => println!("Received command \"{}\" is not supported\n", input_command)
+        if is_metacommand(input_command) {
+            match input_command {
+                ".quit" | ".q" => {
+                        println!("Closing down application. Bye\n");
+                        break;
+                    },
+                ".help" | ".h" => {
+                    print_help();
+                    continue;
+                    },
+                _ => println!("Received metacommand \"{}\" is not supported\n", input_command)
+            }
+        } else {
+            sql_parser(input_command)
         }
 
     }
