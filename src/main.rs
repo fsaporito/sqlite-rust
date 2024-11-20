b0143d0b4908e69ef7ada1afe04a0be6d127f524
fsaporito <sap93@gmail.com>
2024-11-20 17:50:43 +0100

    Add REPL

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,46 @@
+use std::io::{self, Write};
 
+fn print_help() {
+    println!("#### SQLITE Rust Clone ###\n");
+    println!("Helper \n");
+    println!("Available Commands:\n");
+    println!("\t help, h => this prompt\n");
+    println!("\t quit, q => quit program prompt\n");
+}
+
+fn print_prompt() {
+    print!("db > ");
+    io::stdout().flush().expect("Failed to flush stdout")
+}
 
 fn main() {
-    println!("Hello World")
+    println!("Hello I am an SQLITE Rust Clone :)\n");
+
+    loop {
+
+        print_prompt();
+
+        let mut input_command = String::new();
+
+        io::stdin()
+        .read_line(&mut input_command)
+        .expect("Error reading input command");
+
+        let input_command = input_command.trim();
+
+        println!("Received command is: {}\n", input_command);
+
+        match input_command {
+            "quit" | "q" => {
+                    println!("Closing down application. Bye\n");
+                    break;
+                },
+            "help" | "h" => {
+                print_help();
+                continue;
+                },
+            _ => println!("Received command \"{}\" is not supported\n", input_command)
+        }
+
+    }
 }
